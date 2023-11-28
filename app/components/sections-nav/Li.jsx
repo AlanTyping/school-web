2ec76917ab2a7b19cb5769011979e45f4249bc62
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-11-28 14:34:57 -0300

    new changes

--- app/components/sections-nav/Li.jsx
+++ app/components/sections-nav/Li.jsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { HashLink as Link } from 'react-router-hash-link';
-
+// import { HashLink as Link } from 'react-router-hash-link';
+import Link from "next/link";
 
 const Li = ({ name, hash, active, setActive }) => {
   return (
     <li className='h-full w-full'>
-      <Link onClick={() => setActive(hash)} to={`${hash}`} smooth
-       className={ `h-full w-full flex justify-center items-center ${active === hash && 'bg-red-400'}`}>
+      <Link onClick={() => setActive(hash)} href={`${hash}`} className={ `h-full w-full flex justify-center items-center ${active === hash && 'bg-red-400'}`}>
         {name}
       </Link>
     </li>
--- app/components/sections-nav/SectionsNav.jsx
+++ app/components/sections-nav/SectionsNav.jsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import { useRouter } from 'next/navigation';
+// import { BrowserRouter } from 'react-router-dom';
+// import { useRouter } from 'next/navigation';
 import Li from './Li';
 
 const li = [
@@ -28,21 +28,19 @@ const SectionsNav = () => {
   const [active, setActive] = useState('asd');
   const [windowWidth, setWindowWidth] = useState(123);
 
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined') {
-  //     setActive(window.location.hash)
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setActive(window.location.hash)
+    }
+  }, []);
 
 
   return (
-    <BrowserRouter>
       <nav className='h-[60px] w-[200px] bottom-[5%] rounded bg-orange-400 z-[2] fixed md:left-6 md:h-[200px] md:w-[60px] md:top-[50%]'>
         <ul className='flex flex-row md:flex-col justify-center text-white h-full w-full rounded-l-lg'>
           {li.map((e) => <Li name={e.name} hash={e.hash} active={active} setActive={setActive} key={e.hash} />)}
         </ul>
       </nav>
-    </BrowserRouter>
   )
 }
 
