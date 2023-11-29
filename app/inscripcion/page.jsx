8d382cb5a07b7e9e725704f4ed1710a72bc157e2
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-11-29 13:29:02 -0300

    google form

--- app/inscripcion/page.jsx
+++ app/inscripcion/page.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/header/Header';
+import Image from 'next/image';
+import logo from './d.png'
 
 const Inscription = () => {
   return (
-    <div>Inscription</div>
+    <div className='flex flex-col items-center justify-center'>
+        <Image className='md:mr-[300px]' src={logo} width={600} />
+
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdlbhhC6SVIJgc0RjAiGT2jmsQIum0OBz_vI-NP23u9pLIaRA/viewform?embedded=true"
+        width="640" height="585" frameborder="0" marginheight="0" marginwidth="0"
+      >Cargando…</iframe>
+    </div>
   )
 }
 
