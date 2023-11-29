8f5970c6d861ab6f6b87929fa4968138dfeaa164
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-11-29 14:08:11 -0300

    new changes

--- app/inscripcion/page.jsx
+++ app/inscripcion/page.jsx
@@ -6,15 +6,16 @@ import logo from './d.png'
 const Inscription = () => {
   return (
     <div className='flex flex-col items-center justify-center w-full relative'>
-      <div className='w-full md:w-[50%] flex items-start'>
-        <Image className='ml-4 h-[150px] w-[250px]' src={logo} />
+      <div className='w-full md:w-[50%] 2xl:w-[33%] flex items-start'>
+        <Image className='ml-4 h-[150px] w-[250px]' alt='school-logo' src={logo} />
       </div>
         
         
 
       <iframe
+        className='w-[100%] md:w-[50%]'
         src="https://docs.google.com/forms/d/e/1FAIpQLSdlbhhC6SVIJgc0RjAiGT2jmsQIum0OBz_vI-NP23u9pLIaRA/viewform?embedded=true"
-        width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0"
+        height="900"
       >Cargando…</iframe>
     </div>
   )
