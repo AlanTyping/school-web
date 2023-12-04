fd0628488180d401871d3e4ddb49ba6e91239a2a
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-04 19:48:28 -0300

    new changes

--- app/sections/section 1/Section.jsx
+++ app/sections/section 1/Section.jsx
@@ -2,18 +2,31 @@ import { GuideNav } from "@/app/components/guide-nav/GuideNav"
 
 const SectionOne = () => {
   return (
-    <div id='section-one' className='h-[720px] w-full text-[#001d3d] relative z-[1] p-6'>
-      <div className="h-[200px] flex items-end justify-center w-full">
+    <div id='section-one' className='h-auto md:h-[720px] w-full md:w-[80%] text-[#001d3d] relative z-[1] px-10 md:px-6 p-6'>
+      <div className="h-[80px] md:h-[140px] flex items-end justify-center w-full">
         <h1 className="text-[2.3rem]">Identidad escolar</h1>
       </div>
 
-      <div className="h-[200px] flex items-center justify-start">
-        <p className="md:w-[50%]">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      <div className="h-auto flex items-center justify-center w-full mt-10">
+        <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
           dolore enim aliquid veniam consectetur animi voluptas quaerat!
         </p>
       </div>
 
+      <div className="h-auto flex items-center justify-start w-full flex-col md:flex-row">
+        <div className="h-[180px] flex-col p-6 md:h-[230px] w-[380px] bg-[#d48611] rounded my-10 flex items-center justify-center">
+          <h2 className="text-[1.5rem] my-[5px]">testimonio</h2>
+          <p className="my-[8px]">"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, veritatis eum excepturi quia quod laborum veniam sed ullam delectus similique voluptates"</p>
+        </div>
+
+        <p className="md:w-[55%] p-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
+          dolore enim aliquid veniam consectetur animi voluptas quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Placeat cum voluptate nemo eos ad repudiandae. Autem, tempore neque labore nobis voluptatem odio,
+           fugiat similique fugit rerum quod inventore, dicta expedita!
+        </p>
+      </div>
     </div>
   )
 }
