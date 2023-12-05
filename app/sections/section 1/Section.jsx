207a0775fbe4f4b62855f4543665eb219b361c7c
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-05 19:11:27 -0300

    new changes

--- app/sections/section 1/Section.jsx
+++ app/sections/section 1/Section.jsx
@@ -1,3 +1,6 @@
+import image from './assets/image3.svg';
+import Image from 'next/image';
+
 const SectionOne = () => {
   return (
     <div className="h-auto w-full flex items-center justify-center">
@@ -7,12 +10,28 @@ const SectionOne = () => {
           <h1 className="text-[2.3rem]">Identidad escolar</h1>
         </div>
 
-        <div className="h-auto flex items-center justify-center w-full mt-10 lg:px-[80px]">
+        <div className="h-auto flex items-center flex-col justify-center w-full mt-10 lg:px-[80px]">
           <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
             dolore enim aliquid veniam consectetur animi voluptas quaerat!
           </p>
         </div>
+
+
+        <div className="h-auto flex items-center justify-center w-full flex-col lg:flex-row">
+          <p className="lg:w-[50%] lg:p-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
+            dolore enim aliquid veniam consectetur animi voluptas quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Placeat cum voluptate nemo eos ad repudiandae. Autem, tempore neque labore nobis voluptatem odio,
+            fugiat similique fugit rerum quod inventore, dicta expedita!
+          </p>
+
+          <div className="h-[180px] flex-col p-6 lg:h-[230px] w-[380px] bg-[#b3b3b3] text-white rounded my-10 flex items-center justify-center">
+            <Image src={image} height={60} width={60} alt />
+          </div>
+        </div>
+
+        <div className='w-full flex-center'><h2>cultura</h2></div>
 
         <div className="h-auto flex items-center justify-center w-full flex-col lg:flex-row">
           <div className="h-[180px] flex-col p-6 lg:h-[230px] w-[380px] bg-[#3d0000] text-white rounded my-10 flex items-center justify-center">
