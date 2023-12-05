1c209c94b9234c36d59109f6f10315b4b0fb3b02
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-05 19:45:13 -0300

    new changes

--- app/sections/section 1/Section.jsx
+++ app/sections/section 1/Section.jsx
@@ -4,42 +4,44 @@ import Image from 'next/image';
 const SectionOne = () => {
   return (
     <div className="h-auto w-full flex items-center justify-center">
-      <div id='section-one' className='h-auto w-full lg:w-[80%] text-[#001d3d] bg-white relative z-[1] px-10 lg:px-6 p-6'>
+      <div id='section-one' className='h-auto w-full md:w-[80%] text-[#001d3d] relative z-[1] px-10 md:px-6 p-6'>
 
-        <div className="h-[80px] lg:h-[140px] flex items-end justify-center w-full">
-          <h1 className="text-[2.3rem]">Identidad escolar</h1>
+        <div className="h-[80px] md:h-[140px] flex items-end justify-center w-full">
+          <h1 className="text-[2rem] md:text-[2.3rem]">Identidad escolar</h1>
         </div>
 
-        <div className="h-auto flex items-center flex-col justify-center w-full mt-10 lg:px-[80px]">
-          <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
-            dolore enim aliquid veniam consectetur animi voluptas quaerat!
-          </p>
-        </div>
-
-
-        <div className="h-auto flex items-center justify-center w-full flex-col lg:flex-row">
-          <p className="lg:w-[50%] lg:p-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        <div className="h-auto flex items-center flex-col justify-center w-full mt-10 px-10">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
             dolore enim aliquid veniam consectetur animi voluptas quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Placeat cum voluptate nemo eos ad repudiandae. Autem, tempore neque labore nobis voluptatem odio,
-            fugiat similique fugit rerum quod inventore, dicta expedita!
+            Placeat cum voluptate nemo eos ad repudiandae.
           </p>
+        </div>
 
-          <div className="h-[180px] flex-col p-6 lg:h-[230px] w-[380px] bg-[#b3b3b3] text-white rounded my-10 flex items-center justify-center">
+
+        <div className="h-auto flex items-center justify-evenly w-full flex-col md:flex-row">
+          <div className="h-[220px] md:order-2 flex-col p-6 md:p-0 md:h-[240px] w-full md:w-[450px] bg-[#b3b3b3] text-white rounded my-10 flex items-center justify-center">
             <Image src={image} height={60} width={60} alt />
           </div>
+
+          <p className="md:w-[45%]">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
+            dolore enim voluptas quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Placeat cum voluptate nemo eos ad repudiandae. Autem, tempore neque labore nobis voluptatem odio,
+            , Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Placeat cum voluptate nemo eos ad repudiandae.
+          </p>
         </div>
 
         <div className='w-full flex-center'><h2>cultura</h2></div>
 
-        <div className="h-auto flex items-center justify-center w-full flex-col lg:flex-row">
-          <div className="h-[180px] flex-col p-6 lg:h-[230px] w-[380px] bg-[#3d0000] text-white rounded my-10 flex items-center justify-center">
+        <div className="h-auto flex items-center justify-center w-full flex-col md:flex-row">
+          <div className="h-[180px] flex-col p-6 md:h-[230px] w-[380px] bg-[#3d0000] text-white rounded my-10 flex items-center justify-center">
             <h2 className="text-[1.5rem] my-[5px]">testimonio</h2>
             <p className="my-[8px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, veritatis eum excepturi quia quod laborum veniam sed ullam delectus similique voluptates</p>
           </div>
 
-          <p className="lg:w-[50%] lg:p-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          <p className="md:w-[50%] md:p-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
             dolore enim aliquid veniam consectetur animi voluptas quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Placeat cum voluptate nemo eos ad repudiandae. Autem, tempore neque labore nobis voluptatem odio,
@@ -47,15 +49,15 @@ const SectionOne = () => {
           </p>
         </div>
 
-        <div className="h-auto flex items-center justify-center w-full flex-col lg:flex-row">
-          <p className="lg:w-[50%] lg:p-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        <div className="h-auto flex items-center justify-center w-full flex-col md:flex-row">
+          <p className="md:w-[50%] md:p-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
             dolore enim aliquid veniam consectetur animi voluptas quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Placeat cum voluptate nemo eos ad repudiandae. Autem, tempore neque labore nobis voluptatem odio,
             fugiat similique fugit rerum quod inventore, dicta expedita!
           </p>
 
-          <div className="h-[180px] flex-col p-6 lg:h-[230px] w-[380px] bg-[#3d0000] text-white rounded my-10 flex items-center justify-center">
+          <div className="h-[180px] flex-col p-6 md:h-[230px] w-[380px] bg-[#3d0000] text-white rounded my-10 flex items-center justify-center">
             <h2 className="text-[1.5rem] my-[5px]">testimonio</h2>
             <p className="my-[8px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, veritatis eum excepturi quia quod laborum veniam sed ullam delectus similique voluptates.</p>
           </div>
