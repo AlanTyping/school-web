1e55fa38c203f8094d7297c3c8b99bb4e2799645
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-06 11:46:06 -0300

    rounded off

--- app/sections/testimonios/testimonio/Testimonio.jsx
+++ app/sections/testimonios/testimonio/Testimonio.jsx
@@ -1,7 +1,7 @@
 
 export const Testimonio = ({ nombre, texto }) => {
   return (
-    <div className='flex flex-col items-center justify-center text-white bg-blue-600 rounded'>
+    <div className='flex flex-col items-center justify-center text-white bg-blue-600'>
       <div className="w-[85vw] md:w-[40vw] flex text-start pt-5 px-10 text-[1.3rem]">
         <h2>{nombre}</h2>
       </div>
