4b2badefe005af81e0844acce3e6fae444041863
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-07 13:35:36 -0300

    new changes

--- app/sections/section 1/Identidad.jsx
+++ app/sections/section 1/Identidad.jsx
@@ -12,7 +12,7 @@ const Identidad = () => {
           <h1 className="text-[2rem] md:text-[2.3rem] 2xl:text-[2.8rem]">Identidad escolar</h1>
         </div>
 
-        <div className="h-auto flex items-center flex-col justify-start w-full 2xl:px-24 mt-4">
+        <div className="h-auto flex items-center flex-col justify-start w-full md:px-10 2xl:px-24 mt-6 md:mt-0 2xl:mt-4">
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Ducimus ut soluta accusantium voluptate quibusdam, nesciunt eum, quam ab eveniet vero magnam exercitationem,
             dolore enim aliquid veniam consectetur animi voluptas quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -21,7 +21,7 @@ const Identidad = () => {
         </div>
 
 
-        <div className="h-auto flex items-center justify-start w-full flex-col 2xl:px-24 md:flex-row">
+        <div className="h-auto flex items-center justify-start w-full flex-col md:px-10 2xl:px-24 md:flex-row">
           <div className="h-[220px] md:order-2 flex-col p-6 md:p-0 md:h-[240px] md:w-[450px] 2xl:h-[400px] 2xl:w-[660px] w-full bg-[#e6e6e6] text-white rounded my-10 flex items-center justify-center">
             <Image src={image} height={60} width={60} alt='identidad' />
           </div>
