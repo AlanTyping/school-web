92711279ff0e8c9e3963f01ef59f320280749767
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-07 17:26:56 -0300

    new changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'school web',
-  description: '',
+  description: 'A showcase project',
 }
 
 export default function RootLayout({ children }) {
