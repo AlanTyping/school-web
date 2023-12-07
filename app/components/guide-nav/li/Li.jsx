92965dd0a07e6861daaaabf18b47bf2c31f42491
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-07 16:46:07 -0300

    new changes

--- app/components/guide-nav/li/Li.jsx
+++ app/components/guide-nav/li/Li.jsx
@@ -26,7 +26,7 @@ const Li = ({ name, hash, index, img }) => {
       className='h-[23vw] w-[26vw] sm:h-[110px] sm:w-[150px] 2xl:h-[150px] 2xl:w-[210px] m-2 sm:m-4 sm:mx-8 rounded bg-[#001d3d] flex items-center justify-center'>
 
       <Link href={`${hash}`} className={`h-full w-full p-6 flex justify-center items-center flex-col 2xl:text-[1.4rem]`}>
-      <Image src={img} alt={`${name}`} className="h-[40px] w-[40px] sm:h-[55px] sm:w-[55px] mb-1 2xl:mb-2" />
+      <Image src={img} alt={`${name}`} className="h-[40px] w-[40px] 2xl:h-[55px] 2xl:w-[55px] mb-1 2xl:mb-2" />
         {name}
       </Link>
       
--- app/sections/course/Course.jsx
+++ app/sections/course/Course.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Course = () => {
   return (
-    <section id='info' className='flex-col flex-center text-white w-[90%] sm:w-[80%] px-7 sm:px-24 py-14'>
+    <section id='info' className='flex-col flex-center text-white w-[90%] sm:w-[80%] px-7 sm:px-24 py-10'>
       <div className='w-full flex-center'>
         <h2 className='text-[2rem] 2xl:text-[2.5rem]'>Info cursada</h2>
       </div>
