d03d59a6f5f2881c0fbd1c9180b6d1ffbe1076aa
AlanTyping <alan.a.n.r.ar@gmail.com>
2024-01-12 01:15:27 -0300

    .

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,7 +1,8 @@
 import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   revalidateTag("posts");
 
-  return Response.json({ message: "hola" })
+  return NextResponse({message: "hola"})
 };
