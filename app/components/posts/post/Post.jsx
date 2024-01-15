f369acad41c52e4b41066f17df33370ef6e99d3e
AlanTyping <alan.a.n.r.ar@gmail.com>
2024-01-15 20:15:41 -0300

    .

--- app/components/posts/post/Post.jsx
+++ app/components/posts/post/Post.jsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 import Image from 'next/image'
 import logo from './svg/school.svg'
 import linkSvg from './svg/link.svg'
-import { Arimo } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const montserrat = Arimo({ subsets: ['latin'], weight: "400" });
+const montserrat = Poppins({ subsets: ['latin'], weight: "300" });
 
 export const Post = ({ fecha, titulo, descripcion, link, imagen }) => {
   return (
@@ -21,22 +21,24 @@ export const Post = ({ fecha, titulo, descripcion, link, imagen }) => {
       </div>
 
 
-      <div className="w-[90%]">
+      <div className="w-[85%] flex flex-col flex-center">
         {imagen && <div className="relative mb-6 h-[160px] sm:h-[250px] w-full">
           <Image style={{ objectFit: "contain" }} quality={65} fill={true} sizes='50vw' className="rounded" alt="" src={imagen} />
         </div>}
 
-        {titulo && <div className={`my-1 text-[1.5rem]`}><h1>{titulo}</h1></div>}
+        {titulo && <div className={`my-1 text-[1.5rem] text-[#eee] w-[97%]`}>
+          <h1>{titulo}</h1>
+        </div>}
 
-        {descripcion && <div className={`my-5 text-[#ddd] flex w-full justify-center text-[1.1rem] ${montserrat.className}`}>
+        {descripcion && <div className={`mt-3 text-[#ddd] flex w-full justify-center text-[1rem] ${montserrat.className}`}>
           <p className='w-[97%]'>{descripcion}</p>
         </div>}
 
-        {link && <div className={`text-[0.85rem] w-[97%] flex justify-start`}>
+        {link && <div className={`text-[0.85rem] mt-5 w-[97%] flex justify-start`}>
           <Link className='flex bg-[#001d3d] rounded p-1 pr-4 flex-row items-center' href={link} target="blank"><Image src={linkSvg} alt='' height={30} width={30} className='mr-[4px]' />{link}</Link>
-          </div>}
+        </div>}
 
-        {fecha && <div className="mb-2 mt-6 justify-end flex text-[#ccc] text-[1.05rem]">
+        {fecha && <div className="mb-2 mt-8 justify-end flex text-[#ccc] text-[1.05rem] w-[97%]">
           <p className='border-b-[2px] border-b-[#ffa500]'>{fecha}</p>
         </div>}
       </div>
