1af2f02cecc1a39aa4a0ee57841cf3fdac5ea45b
AlanTyping <alan.a.n.r.ar@gmail.com>
2024-01-19 14:59:32 -0300

    .

--- app/components/posts/post/Post.jsx
+++ app/components/posts/post/Post.jsx
@@ -11,7 +11,7 @@ const figtreeBold = Figtree({ subsets: ['latin'], weight: "600" });
 
 export const Post = ({ fecha, titulo, descripcion, link, imagen, i }) => {
   return (
-    <div className={`${i > 2 && 'hidden'} overflow-y-auto mx-5 w-[80%] max-h-[400px] flex flex-col max-w-[400px] pb-4 pt-6 min-w-[325px] items-center text-white bg-[#1a3275] my-5 rounded`}>
+    <div className={`${i > 2 && 'hidden'} overflow-y-auto mx-5 w-[80%] max-h-[300px] flex flex-col max-w-[400px] pb-4 pt-6 min-w-[325px] items-center text-white bg-[#1a3275] my-5 rounded`}>
 
       {
         <div className="w-[90%] flex items-center flex-row mb-5">
@@ -26,7 +26,7 @@ export const Post = ({ fecha, titulo, descripcion, link, imagen, i }) => {
 
 
 
-      {/* <div className="w-[90%] flex flex-col flex-center">
+      {/* 
         <div className="flex flex-col flex-center w-full">
           {imagen && <div className="relative h-[150px] sm:h-[210px] w-full mb-1">
             <Image style={{ objectFit: "contain" }} quality={65} fill={true} sizes='50vw' className="rounded" alt="" src={imagen} />
@@ -42,13 +42,15 @@ export const Post = ({ fecha, titulo, descripcion, link, imagen, i }) => {
         </div> */}
 
       <div className="w-[90%] flex flex-col flex-center">
-        {imagen && <div className="relative mb-5 h-[150px] sm:h-[210px] w-full mb-1">
-          <Image style={{ objectFit: "contain" }} quality={65} fill={true} sizes='50vw' className="rounded" alt="" src={imagen} />
-        </div>}
+        <div className="flex flex-col-reverse flex-center w-full">
+          {imagen && <div className="relative h-[150px] sm:h-[210px] w-full mb-1">
+            <Image style={{ objectFit: "contain" }} quality={65} fill={true} sizes='50vw' className="rounded" alt="" src={imagen} />
+          </div>}
 
-        {titulo && <div className={`mb-5 text-[1.5rem] text-[#eee] flex w-[97%] ${figtreeBold.className}`}>
-          <h1>{titulo}</h1>
-        </div>}
+          {titulo && <div className={`mb-2 text-[1.5rem] text-[#eee] flex w-[97%] ${figtreeBold.className}`}>
+            <h1>{titulo}</h1>
+          </div>}
+        </div>
 
 
         {descripcion && <div className={`text-[#ddd] flex w-full justify-center text-[1rem] ${figtree.className}`}>
@@ -59,7 +61,7 @@ export const Post = ({ fecha, titulo, descripcion, link, imagen, i }) => {
           <Link className='flex bg-[var(--bg)] rounded p-1 pr-4 flex-row items-center' href={link} target="blank"><Image src={linkSvg} alt='' height={30} width={30} className='mr-[4px]' />{link}</Link>
         </div>}
 
-        
+
         {fecha && <div className="justify-end mt-4 flex text-[#ccc] text-[1rem] w-[97%]">
           <p className={`${figtree.className}`}>{fecha}</p>
         </div>}
