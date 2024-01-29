ba0631966dbbfcc66232a7046b345e7a7150373c
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-01-29 20:00:03 -0300

    .

--- app/posts/PostsApi.jsx
+++ app/posts/PostsApi.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { api } from '@/app/api/api';
 import { Posts } from './Posts';
+import { unstable_noStore as noStore } from 'next/cache';
 
+noStore()
 export default async function PostsApi () {
+  noStore()
   const posts = await api.posts();
   
   return <Posts posts={posts} />
