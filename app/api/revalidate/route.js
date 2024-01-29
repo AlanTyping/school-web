72585f4039a17d38854e09b358c80faf65100eda
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-01-29 16:08:31 -0300

    .

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -2,9 +2,11 @@ import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 import { unstable_noStore as noStore } from 'next/cache';
 
+export const revalidate = 0;
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
+  noStore()
   revalidateTag("posts");
 
   return NextResponse.json({revalidated: true}, {
