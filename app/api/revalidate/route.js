5a19dd75da47c6048ceeb60b462b696bd248b7e4
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-01-29 13:56:16 -0300

    .

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,6 +1,8 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET() {
+  revalidatePath('/');
+  revalidatePath('/posts');
 
   return Response.json({ revalidated: true });
 };
