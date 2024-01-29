245304f4c43debd22ed5bed437cd0b078e41be0e
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-01-29 15:11:16 -0300

    .

--- app/api/api.js
+++ app/api/api.js
@@ -2,8 +2,7 @@ export const api = {
   posts: async () => {
     const timestamp = new Date().getTime();
 
-    return fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRgc7lbWkLpzlhRSBXIz7d3_yWJIEIzs3UZSPS1keA4COtK4SI-Yn5LK63o-sH2TgRASVD3KwzoNCW0/pub?output=tsv&timestamp=${timestamp}`, {
-      cache: "no-cache",
+    return fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRgc7lbWkLpzlhRSBXIz7d3_yWJIEIzs3UZSPS1keA4COtK4SI-Yn5LK63o-sH2TgRASVD3KwzoNCW0/pub?output=tsv`, {
       next: { tags: ['posts']}})
     .then((jiji) => jiji.text())
       .then((data) => data.split('\n').slice(1).map((row) => {
