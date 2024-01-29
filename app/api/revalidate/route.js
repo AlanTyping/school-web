c5cb1f4f7e9d913225a3ba5c7352faf872495d6a
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-01-29 16:01:36 -0300

    .

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -2,6 +2,8 @@ import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 import { unstable_noStore as noStore } from 'next/cache';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   revalidateTag("posts");
 
