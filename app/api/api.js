27568b7216cd50445fd60a48c5964afad87ab9c7
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-01-29 14:31:28 -0300

    .

--- app/api/api.js
+++ app/api/api.js
@@ -2,7 +2,9 @@ export const api = {
   posts: async () => {
     const timestamp = new Date().getTime();
 
-    return fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRgc7lbWkLpzlhRSBXIz7d3_yWJIEIzs3UZSPS1keA4COtK4SI-Yn5LK63o-sH2TgRASVD3KwzoNCW0/pub?output=tsv`)
+    return fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRgc7lbWkLpzlhRSBXIz7d3_yWJIEIzs3UZSPS1keA4COtK4SI-Yn5LK63o-sH2TgRASVD3KwzoNCW0/pub?output=tsv`,{
+      cache:"no-cache"
+    })
     .then((jiji) => jiji.text())
       .then((data) => data.split('\n').slice(1).map((row) => {
         const [fecha, titulo, descripcion, imagen, link, categori] = row.split('\t');
