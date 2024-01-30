40871e64e88ed258e1dc66a196508cecdc712a11
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-01-30 00:48:39 -0300

    .

--- app/posts/components/post-header/filter/Filter.jsx
+++ app/posts/components/post-header/filter/Filter.jsx
@@ -20,22 +20,22 @@ export default function Filter({ filterPosts }) {
 
   return (
     <div className="w-full text-[#eee] flex justify-center flex-col items-end pb-2 pt-2">
-      <div onClick={() => setFilter(!filter)} className="mr-8 w-[120px] rounded py-2 bg-[var(--darkBg)]" >
+      <div onClick={() => setFilter(!filter)} className="mr-8 w-[220px] rounded py-2 bg-[var(--darkBg)]" >
         <h1 className="text-[1.1rem] text-center cursor-pointer">Filtrar</h1>
 
         <div className="relative">
           {filter &&
             <div
-              className="flex p-2 items-start flex-col absolute top-0 h-auto pb-3 w-full right-0 
+              className="flex p-2 items-start flex-col absolute w-full top-0 h-auto pb-3 right-0 
               bg-[var(--darkBg)] z-10 rounded">
               {categorias.map(({ borde, categoria, color }, i) => (
 
                 <button key={i} onClick={() => filterPosts(categoria)} className="flex-center flex-row rounded py-2 my-1 bg-[var(--bg)] pr-2 pl-1 relative">
-                  <div className="flex-center w-[20px]">
-                    <div className={`bg-[${color}] border-[${borde}] h-[10px] rounded-[50%] w-[10px]  border-[2px]`} />
+                  <div className="flex-center w-[25px]">
+                    <div className={`bg-[${color}] border-[${borde}] h-[12px] rounded-[50%] w-[12px]  border-[2px]`} />
                   </div>
                   <div className="relative flex-center">
-                    <span className={`text-[0.75rem] ${figtreeBold.className}`}>{categoria}</span>
+                    <span className={`text-[0.95rem] ${figtreeBold.className}`}>{categoria}</span>
                   </div>
                 </button>
                 
