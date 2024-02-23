096c262678b15953a4ae8a5a28666628b41ba00c
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-02-23 13:50:20 -0300

    .

--- app/calendar/components/day/Day.jsx
+++ app/calendar/components/day/Day.jsx
@@ -18,7 +18,7 @@ export default function Day({ day, monthStart, todaysDate, eventos, selectedDate
   return (
     <div
       onClick={() => setSelectedDate(day)}
-      className={`day py-2 ${figtree.className} px-3 ${isSameDay(day, selectedDate) ? 'border-[var(--lightContrast)] border-b-[3px]' : isSameDay(day, todaysDate) ? 'border-[var(--darkBg)] border-b-[3px] bg-[var(--lightContrast)] text-white' : diaSinClases.length >= 1 ? 'border-[#fa6060] border-b-[3px] bg-[#febfbf]' : bg.length >= 1 && 'border-[var(--lightBorder)] border-b-[3px] bg-[var(--lightBg)]'} ${!isSameMonth(day, monthStart) && 'outside-month'} hover:cursor-pointer`}
+      className={`day py-2 ${figtree.className} px-3 ${isSameDay(day, selectedDate) ? diaSinClases.length >= 1 ? 'border-[#fa6060] border-b-[3px]' : bg.length >= 1 ? 'border-[var(--lightBorder)] border-b-[3px]' : 'border-[var(--lightContrast)] border-b-[3px]' : isSameDay(day, todaysDate) ? 'border-[var(--darkBg)] border-b-[3px] bg-[var(--lightContrast)] text-white' : diaSinClases.length >= 1 ? 'border-[#fa6060] border-b-[3px] bg-[#febfbf]' : bg.length >= 1 && 'border-[var(--lightBorder)] border-b-[3px] bg-[var(--lightBg)]'} ${!isSameMonth(day, monthStart) && 'outside-month'} hover:cursor-pointer`}
     >
       {format(day, 'd')}
     </div>
